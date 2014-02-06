bdb80a16a7c83efdf763c128c983bba918c7e3a1
Zhukov <bag4spam@gmail.com>
2014-02-06 16:36:45 +0400

    Single program copy run

--- VolumeHotKey/Program.cs
+++ VolumeHotKey/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
 
 namespace VolumeHotKey
 {
@@ -13,9 +15,19 @@ namespace VolumeHotKey
         [STAThread]
         static void Main()
         {
+            bool isNew;
+            Mutex mutex = new Mutex(true, "VOLUME_CONTROL_HOTKEY_MUTEX", out isNew);
+            if(!isNew)
+            {
+                MessageBox.Show("В памяти компьютера уже загружен один экземпляр данного приложения", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Process.GetCurrentProcess().Kill();
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SettingsForm());
+
+            mutex.ReleaseMutex();
         }
     }
 }
